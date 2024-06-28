ca1bee6e4c555d759f34f74fef314b705e95e66c
sachinjadhav <sachinjdv7@gmail.com>
2024-06-28 17:08:26 +0530

    useref example

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import NavBar from "./NavBar";
-import Cart from "./Cart";
+import Form from "./components/Form";
 
 function App() {
-  const [cart, setCart] = useState(["product1", "product2"]);
-  return (
-    <>
-      <NavBar itemCount={cart.length} />
-      <Cart cartItem={cart} onClear={() => setCart([])} />
-    </>
-  );
+  return <Form />;
 }
 
 export default App;
