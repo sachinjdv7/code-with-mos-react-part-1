494511678a0563dbf618762cf03bc9d80bd9810d
sachinjadhav <sachinjdv7@gmail.com>
2024-07-01 20:09:09 +0530

    loader added

--- src/FetchUser.tsx
+++ src/FetchUser.tsx
@@ -9,16 +9,22 @@ interface User {
 const FetchUser = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const controller = new AbortController();
+    setIsLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
@@ -26,6 +32,8 @@ const FetchUser = () => {
   return (
     <div>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
